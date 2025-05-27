21750c16f2a71042309f4310af64d17df1660c92
Georgio-Azar <georgio.azar@ikattan.com>
2025-05-27 10:38:10 +0200

    feat: Addition of the redirection in the profile page

--- src/components/pages/Profile.tsx
+++ src/components/pages/Profile.tsx
@@ -2,13 +2,21 @@ import { useSelector } from "react-redux";
 import type { RootState } from "../../store/store";
 import { useEffect } from "react";
 
+interface UserData {
+    nom: string;
+    prenom: string;
+    email: string;
+    age: number;
+    id: string;
+}
+
 export default function Profile() {
     const userId = useSelector((state: RootState) => state.auth.userId);
     const accessToken = useSelector((state: RootState) => state.auth.accessToken);
     console.log("User ID from Redux store:", userId);
     console.log("Access Token from Redux store:", accessToken);
 
-    let userData: any = null;
+    let userData: UserData;
     // Fetch user data from the API using the access token
     useEffect(() => {
         const fetchUserData = async () => {
@@ -20,6 +28,11 @@ export default function Profile() {
                         "Content-Type": "application/json",
                     },
                 });
+                if (response.status === 403) {
+                    // Redirect to login page
+                    window.location.href = "/login";
+                    return;
+                }
                 if (!response.ok) {
                     throw new Error("Failed to fetch user data");
                 }
@@ -35,7 +48,6 @@ export default function Profile() {
     return (
     <div id="profile">
         <h1>Profile</h1>
-
     </div>
     );    
 }
